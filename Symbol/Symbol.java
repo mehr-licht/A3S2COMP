80d62218e25c115af925a86cb5a17faf4641ad6f
Luis Ricardo Marques Oliveira <up201607946@fe.up.pt>
2019-04-05 11:24:18 +0100

    beginning Symbol Table

---
+++ Symbol/Symbol.java
@@ -0,0 +1,28 @@
+package Symbol;
+
+public class Symbol {
+  private String name;
+  private String type;
+  private int size;
+
+  public Symbol(String name, String type) {
+    this.name = name;
+    this.type = type;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public int getSize() {
+    return this.size;
+  }
+
+  public void setSize(int n) {
+    this.size = n;
+  }
+}
